cb6609d9615fc7c3bbc99c4f17ebe20202e8da34
kazumi <kazumi121589@yahoo.co.jp>
2022-08-22 22:04:58 +0900

    code generatorでAPIコードを生成した

--- orval.config.js
+++ orval.config.js
@@ -1,21 +1,21 @@
 module.exports = {
   payreco: {
     output: {
-      mode: 'tags-split',
-      target: 'src/api/endpoints.ts',
-      schemas: 'src/api/model',
-      client: 'react-query',
+      mode: "tags-split",
+      target: "src/api/endpoints.ts",
+      schemas: "src/api/model",
+      client: "react-query",
       mock: true,
       override: {
         useDates: true,
         mutator: {
-          path: './src/api/mutator/custom-instance.ts',
-          name: 'customInstance',
+          path: "./src/api/custom-instance.ts",
+          name: "customInstance",
         },
       },
     },
     input: {
-      target: './../backend/openapi.yml',
+      target: "./../backend/openapi.yml",
     },
   },
 };
