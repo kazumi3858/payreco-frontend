b2dc092268495cdf2fe03e58ac0cef27a6959db5
kazumi <kazumi121589@yahoo.co.jp>
2022-08-24 12:46:33 +0900

    カレンダーを設置した

--- src/components/organisms/Calendar.tsx
+++ src/components/organisms/Calendar.tsx
@@ -1,7 +1,195 @@
+import {
+  add,
+  eachDayOfInterval,
+  endOfMonth,
+  format,
+  getDay,
+  isEqual,
+  isSameDay,
+  isSameMonth,
+  isToday,
+  parse,
+  parseISO,
+  startOfToday,
+} from "date-fns";
+import ja from "date-fns/locale/ja";
+import { Key, useState } from "react";
+import DetailedSchedule from "./DetailedSchedule";
+
+const meetings = [
+  {
+    id: 1,
+    name: "株式会社××",
+    startDatetime: "2022-08-11T13:00",
+    endDatetime: "2022-08-11T14:30",
+  },
+  {
+    id: 2,
+    name: "株式会社××",
+    startDatetime: "2022-08-20T09:00",
+    endDatetime: "2022-08-20T11:30",
+  },
+  {
+    id: 3,
+    name: "株式会社××",
+    startDatetime: "2022-08-20T17:00",
+    endDatetime: "2022-08-20T18:30",
+  },
+  {
+    id: 4,
+    name: "株式会社××",
+    startDatetime: "2022-08-09T13:00",
+    endDatetime: "2022-08-09T14:30",
+  },
+  {
+    id: 5,
+    name: "株式会社××",
+    startDatetime: "2022-08-13T14:00",
+    endDatetime: "2022-08-13T14:30",
+  },
+];
+
+const colStartClasses = [
+  "",
+  "col-start-2",
+  "col-start-3",
+  "col-start-4",
+  "col-start-5",
+  "col-start-6",
+  "col-start-7",
+];
+
 function Calendar() {
+  const today = startOfToday();
+  const [selectedDay, setSelectedDay] = useState(today);
+  const [currentMonth, setCurrentMonth] = useState(format(today, "MMM-yyyy"));
+  const firstDayCurrentMonth = parse(currentMonth, "MMM-yyyy", new Date());
+
+  const days = eachDayOfInterval({
+    start: firstDayCurrentMonth,
+    end: endOfMonth(firstDayCurrentMonth),
+  });
+  function classNames(...classes: (string | boolean)[]) {
+    return classes.filter(Boolean).join(" ");
+  }
+
+  function previousMonth() {
+    const firstDayNextMonth = add(firstDayCurrentMonth, { months: -1 });
+    setCurrentMonth(format(firstDayNextMonth, "MMM-yyyy"));
+  }
+
+  function nextMonth() {
+    const firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 });
+    setCurrentMonth(format(firstDayNextMonth, "MMM-yyyy"));
+  }
+
+  const selectedDayMeetings = meetings.filter((meeting) =>
+    isSameDay(parseISO(meeting.startDatetime), selectedDay)
+  );
+
   return (
-    <div className="h-96">
-      <p>calendar</p>
+    <div className="pt-16">
+      <div className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6">
+        <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
+          <div className="md:pr-14">
+            <div className="flex items-center">
+              <h2 className="flex-auto font-semibold text-gray-900">
+                {format(firstDayCurrentMonth, "yyyy年 MMMM", { locale: ja })}
+              </h2>
+              <button
+                type="button"
+                onClick={previousMonth}
+                className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+              >
+                <span>{"<"}</span>
+              </button>
+              <button
+                onClick={nextMonth}
+                type="button"
+                className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+              >
+                <span>{">"}</span>
+              </button>
+            </div>
+            <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500">
+              <div>日</div>
+              <div>月</div>
+              <div>火</div>
+              <div>水</div>
+              <div>木</div>
+              <div>金</div>
+              <div>土</div>
+            </div>
+            <div className="grid grid-cols-7 mt-2 text-sm">
+              {days.map((day, dayIdx) => (
+                <div
+                  key={day.toString()}
+                  className={classNames(
+                    dayIdx === 0 && colStartClasses[getDay(day)],
+                    "py-1.5"
+                  )}
+                >
+                  <button
+                    type="button"
+                    onClick={() => setSelectedDay(day)}
+                    className={classNames(
+                      isEqual(day, selectedDay) && "text-white",
+                      !isEqual(day, selectedDay) &&
+                        isToday(day) &&
+                        "text-red-500",
+                      !isEqual(day, selectedDay) &&
+                        !isToday(day) &&
+                        isSameMonth(day, firstDayCurrentMonth) &&
+                        "text-gray-900",
+                      !isEqual(day, selectedDay) &&
+                        !isToday(day) &&
+                        !isSameMonth(day, firstDayCurrentMonth) &&
+                        "text-gray-400",
+                      isEqual(day, selectedDay) && isToday(day) && "bg-red-500",
+                      isEqual(day, selectedDay) &&
+                        !isToday(day) &&
+                        "bg-gray-900",
+                      !isEqual(day, selectedDay) && "hover:bg-gray-200",
+                      (isEqual(day, selectedDay) || isToday(day)) &&
+                        "font-semibold",
+                      "mx-auto flex h-8 w-8 items-center justify-center rounded-full"
+                    )}
+                  >
+                    <time dateTime={format(day, "yyyy-MM-dd")}>
+                      {format(day, "d")}
+                    </time>
+                  </button>
+
+                  <div className="w-1 h-1 mx-auto mt-1">
+                    {meetings.some((meeting) =>
+                      isSameDay(parseISO(meeting.startDatetime), day)
+                    ) && (
+                      <div className="w-1 h-1 rounded-full bg-sky-500"></div>
+                    )}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <section className="mt-12 md:mt-0 md:pl-14">
+            <h2 className="font-semibold text-gray-900">
+              <time dateTime={format(selectedDay, "yyyy-MM-dd")}>
+                {format(selectedDay, "MMM dd日", { locale: ja })}
+              </time>
+              の予定
+            </h2>
+            <ol className="mt-4 space-y-1 text-sm leading-6 text-gray-500">
+              {selectedDayMeetings.length > 0 ? (
+                selectedDayMeetings.map((meeting) => (
+                  <DetailedSchedule meeting={meeting} key={meeting.id} />
+                ))
+              ) : (
+                <p>予定はありません。</p>
+              )}
+            </ol>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/organisms/DetailedSchedule.tsx
+++ src/components/organisms/DetailedSchedule.tsx
@@ -1,8 +1,24 @@
-function DetailedSchedule() {
+import { format, parseISO } from "date-fns";
+
+function DetailedSchedule({ meeting }: { meeting: any }): JSX.Element {
+  const startDateTime = parseISO(meeting.startDatetime);
+  const endDateTime = parseISO(meeting.endDatetime);
+
   return (
-    <>
-      <p>detailed schedule</p>
-    </>
+    <li className="flex items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-gray-100 hover:bg-gray-100">
+      <div className="flex-auto">
+        <p className="text-gray-900">{meeting.name}</p>
+        <p className="mt-0.5">
+          <time dateTime={meeting.startDatetime}>
+            {format(startDateTime, "h:mm a")}
+          </time>
+          {" - "}
+          <time dateTime={meeting.endDatetime}>
+            {format(endDateTime, "h:mm a")}
+          </time>
+        </p>
+      </div>
+    </li>
   );
 }
 
