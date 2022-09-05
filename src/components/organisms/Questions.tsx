2861fca762e82536985fab1a0dc27879463d8e2f
kazumi <kazumi121589@yahoo.co.jp>
2022-09-05 22:16:42 +0900

    クライアント側の退会処理の実装をした

--- src/components/organisms/Questions.tsx
+++ src/components/organisms/Questions.tsx
@@ -1,12 +1,43 @@
+import { auth, provider } from "auth/firebase";
+import { deleteUser, reauthenticateWithPopup } from "firebase/auth";
 import Button from "components/atoms/Button";
 import Heading from "components/atoms/Heading";
+import router from "next/router";
 
 function Questions() {
+  const user = auth.currentUser;
+
+  const reauthenticate = () => {
+    user &&
+      reauthenticateWithPopup(user, provider)
+        .then(() => {})
+        .catch((error) => {
+          console.log(error);
+        });
+  };
+
+  const deleteAccount = () =>
+    user &&
+    deleteUser(user)
+      .then(() => {
+        console.log("An account has been deleted.");
+        router.push("/");
+        alert('退会処理が完了しました。ご利用いただきましてありがとうございました。')
+      })
+      .catch((error) => {
+        if (error.code === "auth/requires-recent-login") {
+          reauthenticate();
+          alert("処理に失敗しました。もう一度お試しください。");
+        } else {
+          console.log(error);
+        }
+      });
+
   const handleClick = () => {
     const confirmation = confirm(
       "全てのデータが消えてしまいますが退会しますか？"
     );
-    confirmation ? console.log("done") : console.log("Canceled");
+    confirmation ? deleteAccount() : console.log("Canceled.");
   };
   return (
     <div className="flex justify-center mt-6">
@@ -15,9 +46,15 @@ function Questions() {
         <h3 className="my-3">退会方法を教えてください。</h3>
         <p>退会処理は以下のボタンから行うことができます。</p>
         <p>
+          ※
           退会処理を行うとこれまでのデータが全て消えてしまいますのでご注意ください。
         </p>
-        <Button text="退会する" onClick={handleClick} />
+        <p>
+          ※ 退会処理を行う際にGooleアカウントの再認証が必要な場合がございます。
+        </p>
+        <div className="my-6 text-center">
+          <Button text="退会する" onClick={handleClick} />
+        </div>
       </div>
     </div>
   );
--- src/components/templates/Login.tsx
+++ src/components/templates/Login.tsx
@@ -1,14 +1,17 @@
 import { auth, provider } from "auth/firebase";
 import { signInWithPopup } from "firebase/auth";
+import { useState } from "react";
 
 function Login() {
+  const [disable, setDisable] = useState(false);
   const googleSignIn = () => {
+    setDisable(true)
     signInWithPopup(auth, provider).catch((error) =>
       console.log(error.message)
     );
   };
 
-  return <button onClick={googleSignIn}>サインイン</button>;
+  return <button disabled={disable} onClick={googleSignIn}>サインイン</button>;
 }
 
 export default Login;
