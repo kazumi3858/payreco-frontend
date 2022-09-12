b3b5e1061f01d17603176b5880fb52cf5eb60d4f
kazumi <kazumi121589@yahoo.co.jp>
2022-09-12 13:48:36 +0900

    commands.tsとfirenbase.tsを修正した

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -5,7 +5,7 @@ import "firebase/compat/firestore";
 import { attachCustomCommands } from "cypress-firebase";
 
 const fbConfig = {
-  apiKey: String(Cypress.env("CYPRESS_FIREBASE_API_KEY")),
+  apiKey: Cypress.env("CYPRESS_FIREBASE_API_KEY") + "",
   authDomain: Cypress.env("CYPRESS_FIREBASE_AUTH_DOMAIN"),
   projectId: Cypress.env("CYPRESS_FIREBASE_PROJECT_ID"),
   storageBucket: Cypress.env("CYPRESS_FIREBASE_STORAGE_BUCKET"),
