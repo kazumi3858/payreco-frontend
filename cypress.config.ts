0958f7392a14d130d193a0ee826ff3b9e4b894f0
kazumi <kazumi121589@yahoo.co.jp>
2022-09-12 13:52:44 +0900

    workflowsがエラーになるため修正した

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,7 @@ import { plugin as cypressFirebasePlugin } from "cypress-firebase";
 export default defineConfig({
   e2e: {
     projectId: "vkw228",
-    baseUrl: "http://localhost:3001",
+    baseUrl: "https://payreco.vercel.app/",
     defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
       cypressFirebasePlugin(on, config, admin);
