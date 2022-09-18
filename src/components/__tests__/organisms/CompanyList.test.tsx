e1f57385514c5553608f9eb4094b737fa417d018
kazumi <kazumi121589@yahoo.co.jp>
2022-09-18 15:14:16 +0900

    CompanyListのテストを書いた

--- src/components/__tests__/organisms/CompanyList.test.tsx
+++ src/components/__tests__/organisms/CompanyList.test.tsx
@@ -1,26 +1,26 @@
 import "@testing-library/jest-dom";
-// import "@testing-library/jest-dom/extend-expect";
 import CompanyList from "components/organisms/CompanyList";
-import { render, screen, waitFor } from "@testing-library/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { setupServer } from "msw/node";
-import { getCompaniesMSW } from "api/companies/companies.msw";
+import * as ReactQuery from "@tanstack/react-query";
+import { render, screen } from "@testing-library/react";
+import { getGetCompaniesMock } from "api/companies/companies.msw";
 
-const server = setupServer(...getCompaniesMSW());
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
-
-const queryClient = new QueryClient();
-const companyList = (
-  <QueryClientProvider client={queryClient}>
-    <CompanyList />
-  </QueryClientProvider>
-);
+jest.mock("@tanstack/react-query", () => {
+  const original: typeof ReactQuery = jest.requireActual(
+    "@tanstack/react-query"
+  );
+  return {
+    ...original,
+    useQuery: () => ({
+      isLoading: false,
+      data: getGetCompaniesMock(),
+    }),
+  };
+});
 
 describe("CompanyList", () => {
   it("can render company list", async () => {
-    render(companyList);
-    //await waitFor(()=>expect(screen.getByText(/株式会社/)).toBeInTheDocument())
+    render(<CompanyList />);
+    expect(screen.getAllByText(/株式会社/)).toBeTruthy();
+    expect(screen.getAllByText(/[時日]給/)).toBeTruthy();
   });
 });
