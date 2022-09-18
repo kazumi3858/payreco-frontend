316ec9410536a09e97441819338a872291577978
kazumi <kazumi121589@yahoo.co.jp>
2022-09-18 18:43:41 +0900

    Menuのテストを書いた

---
+++ src/components/__tests__/organisms/Menu.test.tsx
@@ -0,0 +1,12 @@
+import "@testing-library/jest-dom";
+import Menu from "components/organisms/Menu";
+import { render, screen } from "@testing-library/react";
+
+describe("Menu", () => {
+  it("can render menu", () => {
+    render(<Menu />);
+    expect(screen.getByText("スケジュール管理")).toBeInTheDocument();
+    expect(screen.getByText("勤務先管理")).toBeInTheDocument();
+    expect(screen.getByText("給料計算")).toBeInTheDocument();
+  });
+});
