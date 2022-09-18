4d5f412a1c471f1b1459b9e6c716db015d350cd1
kazumi <kazumi121589@yahoo.co.jp>
2022-09-18 14:11:34 +0900

    IncomeList.testのファイルを作成した

---
+++ src/components/__tests__/organisms/IncomeList.test.tsx
@@ -0,0 +1,7 @@
+import "@testing-library/jest-dom";
+import IncomeList from "components/organisms/IncomeList";
+import { fireEvent, render, screen } from "@testing-library/react";
+
+describe("IncomeList", () => {
+  it("can call click event", () => {});
+});
