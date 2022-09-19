1bc11fac6d2c356a500d38e1adf836d98f464eae
kazumi <kazumi121589@yahoo.co.jp>
2022-09-19 14:53:34 +0900

    WorkListのテストを書いた

--- src/api/companies/companies.msw.ts
+++ src/api/companies/companies.msw.ts
@@ -37,10 +37,6 @@ export const getCompanyMock = [
 
 export const getCompaniesMSW = () => [
   rest.get("*/companies", (_req, res, ctx) => {
-    return res(
-      ctx.delay(1000),
-      ctx.status(200, "Mocked status"),
-      ctx.json(getGetCompaniesMock())
-    );
+    return res(ctx.json(getCompanyMock));
   }),
 ];
--- src/components/__tests__/organisms/WorkList.test.tsx
+++ src/components/__tests__/organisms/WorkList.test.tsx
@@ -1,39 +1,46 @@
 import "@testing-library/jest-dom";
 import WorkList from "components/organisms/WorkList";
-import * as ReactQuery from "@tanstack/react-query";
 import { render, screen } from "@testing-library/react";
 import { getSelectedWorksMock } from "api/works/works.msw";
-import { getCompanyMock } from "api/companies/companies.msw";
+import { getCompaniesMSW } from "api/companies/companies.msw";
 import { Work } from "api/model";
+import { setupServer } from "msw/node";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import * as firebaseAuth from "firebase/auth";
 
+jest.mock("firebase/auth", () => {
+  const original: typeof firebaseAuth = jest.requireActual("firebase/auth");
+  return { ...original, auth: jest.fn() };
+});
+
+const server = setupServer(...getCompaniesMSW());
 const works = getSelectedWorksMock();
 const thisYear = new Date().getFullYear();
 const thisMonth = new Date().getMonth();
-
-jest.mock("@tanstack/react-query", () => {
-  const original: typeof ReactQuery = jest.requireActual(
-    "@tanstack/react-query"
-  );
-  return {
-    ...original,
-    useQuery: () => ({
-      isLoading: false,
-      data: getCompanyMock,
-    }),
-  };
-});
+const queryClient = new QueryClient();
 
 const workList = (works?: Work[]) => (
-  <WorkList
-    selectedDay={new Date(thisYear, thisMonth, 1)}
-    selectedDayWorks={works}
-  />
+  <QueryClientProvider client={queryClient}>
+    <WorkList
+      selectedDay={new Date(thisYear, thisMonth, 1)}
+      selectedDayWorks={works}
+    />
+  </QueryClientProvider>
 );
 
 describe("WorkList", () => {
-  it("can render work list in selected day", () => {
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
+  it("can render loading", async () => {
     render(workList(works));
-    expect(screen.getAllByText("株式会社山田")).toHaveLength(3);
+    expect(screen.getAllByText(/Loading/)).toBeTruthy();
+  });
+
+  it("can render work list in selected day", async () => {
+    render(workList(works));
+    expect(await screen.findAllByText(/株式会社山田/)).toHaveLength(3);
     expect(screen.getByText(/8:00 AM - 12:00 PM/)).toBeInTheDocument();
     expect(screen.getByText(/12:00 PM - 4:00 PM/)).toBeInTheDocument();
   });
