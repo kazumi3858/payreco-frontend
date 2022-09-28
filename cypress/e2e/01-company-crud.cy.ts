c491c1318ef0919b4db74ff6de561306c4c47a28
kazumi <kazumi121589@yahoo.co.jp>
2022-09-28 22:06:04 +0900

    WorkListテストを修正した

--- cypress/e2e/01-company-crud.cy.ts
+++ cypress/e2e/01-company-crud.cy.ts
@@ -37,7 +37,7 @@ describe("Company CRUD function", () => {
     cy.get("#wage").type("15");
     cy.get("select").select("米ドル");
     cy.contains("input", "保存").click();
-    cy.contains("保存").should('not.exist')
+    cy.contains("保存").should("not.exist");
     cy.contains("株式会社ジキュウ").should("have.length", 1);
   });
 
@@ -49,7 +49,7 @@ describe("Company CRUD function", () => {
     cy.get("#name").clear().type("株式会社ニッキュウ");
     cy.contains("日給制").click();
     cy.contains("input", "保存").click();
-    cy.contains("保存").should('not.exist')
+    cy.contains("保存").should("not.exist");
     cy.contains("株式会社ニッキュウ");
     cy.contains("日給制");
   });
@@ -59,7 +59,7 @@ describe("Company CRUD function", () => {
     cy.contains("株式会社ニッキュウ").should("have.length", 1);
     cy.contains("button", "削除").click();
     cy.contains("button", "はい").click();
-    cy.contains("はい").should('not.exist')
+    cy.contains("はい").should("not.exist");
     cy.contains("株式会社ニッキュウ").should("have.length", 0);
   });
 
