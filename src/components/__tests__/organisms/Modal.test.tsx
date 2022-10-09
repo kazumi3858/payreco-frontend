c0d69fe6db7a8daeeb40dda434c1c5fccdff02ca
kazumi <kazumi121589@yahoo.co.jp>
2022-10-09 13:58:12 +0900

    Modalのテストのリファクタリングをした

--- src/components/__tests__/organisms/Modal.test.tsx
+++ src/components/__tests__/organisms/Modal.test.tsx
@@ -1,37 +1,23 @@
 import "@testing-library/jest-dom";
 import Modal from "components/organisms/Modal";
 import { render, screen } from "@testing-library/react";
-import { useState as useStateMock } from "react";
-
-jest.mock("react", () => ({
-  ...jest.requireActual("react"),
-  useState: jest.fn(),
-}));
-
-const setState = jest.fn();
 
 describe("Modal", () => {
-  beforeEach(() => {
-    (useStateMock as jest.Mock).mockImplementation((init) => [init, setState]);
-  });
-
-  it("should not render modal when user has not opened", async () => {
-    const [modal, setModal] = useStateMock(false);
+  it("cannot render content when modal closed", async () => {
     render(
-      <Modal modal={modal} setModal={setModal}>
-        <p>children</p>
+      <Modal modal={false} setModal={jest.fn()}>
+        <p>content</p>
       </Modal>
     );
-    expect(screen.queryByText("children")).toBeNull();
+    expect(screen.queryByText("content")).toBeNull();
   });
 
-  it("can render modal when user open", async () => {
-    const [modal, setModal] = useStateMock(true);
+  it("can render content when modal opened", async () => {
     render(
-      <Modal modal={modal} setModal={setModal}>
-        <p>children</p>
+      <Modal modal={true} setModal={jest.fn()}>
+        <p>content</p>
       </Modal>
     );
-    expect(screen.getByText("children")).toBeInTheDocument();
+    expect(screen.getByText("content")).toBeInTheDocument();
   });
 });
