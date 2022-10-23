26651d7bb8877a90582af578b2dd8f08e70b88c4
kazumi <kazumi121589@yahoo.co.jp>
2022-10-23 15:02:04 +0900

    メインコンテンツのmarginを調整した

--- src/components/organisms/Calendar.tsx
+++ src/components/organisms/Calendar.tsx
@@ -61,8 +61,8 @@ function Calendar() {
   );
 
   return (
-    <div className="pt-5 pb-10 md:px-4">
-      <div className="mx-auto max-w-lg px-4 md:max-w-7xl">
+    <div className="px-4 pt-5 pb-10">
+      <div className="mx-auto max-w-lg md:max-w-7xl">
         <div className="md:grid md:grid-cols-2">
           <div>
             <div className="rounded-xl border border-stone-300 bg-white p-6 md:mb-10">
