bee7afb9596a0c533b6c258ec93c657425d9e47b
kazumi3858 <kazumi121589@yahoo.co.jp>
2024-04-13 20:26:21 +0900

    ログアウト処理コメントアウト

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,22 +7,24 @@ import { useDeleteUser } from "api/default/default";
 import { auth } from "auth/firebase";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient();
+  const client = new QueryClient();
   const deleteCurrentUser = useDeleteUser();
 
   // サービス終了のため、ページにアクセスがあると強制ログアウトします
-  useEffect(()=> {
-    deleteCurrentUser.mutate();
-    auth.signOut();
-  }, [])
+  // useEffect(()=> {
+  //   deleteCurrentUser.mutate();
+  //   auth.signOut();
+  // }, [])
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
+      <QueryClientProvider client={client}>
       <SiteHead />
-      <div className="font-body leading-8 tracking-wide text-[#4C4D4D]">
-        <Component {...pageProps} />
-      </div>
-    </QueryClientProvider>
+        <div className="font-body leading-8 tracking-wide text-[#4C4D4D]">
+          <Component {...pageProps} />
+        </div>
+      </QueryClientProvider>
+    </>
   );
 }
 
